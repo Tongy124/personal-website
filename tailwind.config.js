ebd8ad33e7cc90404dac1f2da802777573d22486
Bruno <33235089+Tongy124@users.noreply.github.com>
2022-03-07 13:58:02 +0000

    Page Setup & Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}", 
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
